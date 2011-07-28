8f74570a6d2abfdffd993c7582a3a6dbe0d48e98
Benoit Thiell <bthiell@cfa.harvard.edu>
2011-07-28 15:22:23 -0400

    Remove test from csv_utils module.

--- ads_refine/csv_utils.py
+++ ads_refine/csv_utils.py
@@ -1,3 +1,8 @@
+"""
+Functions to deal with CSV escape and unescape.
+See: http://en.wikipedia.org/wiki/Comma-separated_values
+"""
+
 def escape_csv(data):
     if '"' in data:
         return '"%s"' % data.replace('"', '""')
@@ -9,24 +14,6 @@ def unescape_csv(data):
         return data[1:-1].replace('""', '"')
     else:
         return data
-
-def test():
-    escaped = [
-            "No quotes",
-            "'Single quotes'",
-            "\"test \"\"a\"\" test\"",
-            "\"\"\"\"",
-            ]
-
-    unescaped = [
-            "No quotes",
-            "'Single quotes'",
-            "test \"a\" test",
-            "\"",
-            ]
-
-    assert([escape_csv(data) for data in unescaped] == escaped)
-    assert([unescape_csv(data) for data in escaped] == unescaped)
 
 if __name__ == '__main__':
     test()
