22830decc7a348a05cd6467db188d36c64a7a628
Benoit Thiell <bthiell@cfa.harvard.edu>
2011-07-28 14:54:40 -0400

    Add test for the creation of project.

--- tests/test_refine_automation.py
+++ tests/test_refine_automation.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 import sys
 import time
 import unittest
@@ -26,12 +28,34 @@ class TestRefineCreation(unittest.TestCase):
         server = refine.Refine(create.SERVER)
         self.project = server.open_project(project_id)
 
+    def get_cell(self, row_number, column):
+        row_number -= 1
+        response = self.project.get_rows(start=row_number, limit=1)
+        return response.rows[0][column]
+
     def test_columns(self):
         self.assertEqual(self.project.columns, ['Original', 'Without email', 'Emails', 'Bibcodes'])
 
     def test_row_number(self):
         response = self.project.get_rows()
-        self.assertEqual(response.total, 17)
+        self.assertEqual(response.total, 18)
+
+    def test_entity_conversion(self):
+        self.assertEqual(self.get_cell(2, 'Without email'), 'Istituto Astronomico, Università "La Sapienza", via G.M. Lancisi 29, I-00161 Roma, Italy'.decode('utf-8'))
+        self.assertEqual(self.get_cell(5, 'Without email'), 'Astronomical Institute, Czechoslovak Academy of Sciences, Ondřejov Observatory, and Astronomical Institute, Brno University.'.decode('utf-8'))
+
+    def test_quotes(self):
+        self.assertEqual(self.get_cell(3, 'Without email'), 'Laboratoire "Astrophysique, Atomes et Molecules", Departement Atomes et Molecules en Astrophysique, Unite associee au CNRS No. 812, Observatoire de Paris-Meudon, 92190 Meudon, France')
+        self.assertEqual(self.get_cell(11, 'Without email'), "Institut d'Astrophysique de Paris 98bis, Bd Arago 75014 Paris, France")
+
+    def test_emails(self):
+        self.assertEqual(eval(self.get_cell(18, 'Emails')), [])
+        self.assertEqual(eval(self.get_cell(10, 'Emails')), ["vittorio@astr1pi.difi.unipi.it"])
+        self.assertEqual(eval(self.get_cell(9, 'Emails')), ["beersatmsupa.pa.msu.edukriessleratmsupa.pa.msu.edu", "tbirdatkula.phsx.ukans.edu"])
+
+    def test_bibcodes(self):
+        self.assertEqual(self.get_cell(11, 'Bibcodes'), '1997A&AS..121..407L,0')
+        self.assertEqual(self.get_cell(17, 'Bibcodes'), '1981MitAG..52..127S,0 1981gjsa.proc..291S,0')
 
     def tearDown(self):
         self.project.delete()
